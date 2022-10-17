e5a205f6c6b5265412c97aa3fcd347987df9985c
Irina <primonik@br.ru>
2022-10-17 21:19:03 +0300

    приветствие любимчика

---
+++ EX5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ириша")
+{
+    Console.WriteLine("Урррааа, привет Ириша!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
